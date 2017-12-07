88d0f396d940d94c44edacd6848467bdfb6970ce
Yury Demidenko <demidenko05@gmail.com>
2017-12-07 11:53:57 +0300

    dependecy from central

--- maven-central-deploy.sh
+++ maven-central-deploy.sh
@@ -5,10 +5,10 @@
 read -p "Really deploy to maven cetral repository  (yes/no)? "
 
 if ( [ "$REPLY" == "yes" ] ) then
-  read -p "Enter paraphrase to sign APK: "
   ssh-add ~/.ssh/demidenko05git
   ssh-add -l
-  mvn release:clean release:prepare release:perform -Dandroid.release=true -Dsignpass="$REPLY" -B -e | tee maven-central-deploy.log
+  read -p "Enter paraphrase to sign APK: "
+  mvn -Darguments="-Prelease -Dandroid.release=true -Dsignpass=$REPLY" release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
   ssh-add -D
 else
   echo 'Exit without deploy'
